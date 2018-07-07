d5ceff35aa81c2877c50ec54161be18238a5a991
b3Nr11d <b3Nr11d@d177272.local>
2018-07-07 21:47:34 +0100

    fix: user doesn't have access to admin auth db

--- buildMongo.sh
+++ buildMongo.sh
@@ -4,4 +4,4 @@ docker-compose down
 docker-compose up -d
 sleep 1
 docker exec db_mongodb  mongo admin /setup/create-admin.js
-docker exec db_mongodb  mongo admin /setup/create-user.js -u admin -p admin --authenticationDatabase admin
+docker exec db_mongodb  mongo myDb /setup/create-user.js -u admin -p admin --authenticationDatabase admin
